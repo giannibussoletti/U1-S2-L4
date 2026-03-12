9606cae75de106ca3fb78bf10d66287412075745
Gianni <BussolettiGianni@gmail.com>
2026-03-12 10:11:40 +0100

    Update funzioni.js

---
+++ scripts/funzioni.js
@@ -0,0 +1,19 @@
+// FUNZIONI in JS!
+
+// Con funzione in un linguaggio di programmazione, si intende un blocco di codice riutilizzabile
+// Questo blocco di codice viene dotato di nome, e dopo la sua definizionepuò
+// venire "invocato" per attivare al suo interno.
+
+// in JS:
+// 1)  prima si "definisce" la funzione con una dichiarazione
+// 2) poi la si "invoca" (esegue, utilizza)
+
+// La mia prima funzione in JS
+
+// per aprire una funzione si crea una variabile con const e dopo = si dichiara function con () e {} subito dopo
+// il codice va inserito nelle graffe
+const bark = function () {
+  // questa sezione tra graffe si definisce "CORPO", body della funzione
+  // in questo blocco si inseriscono  di JS che voglio "incapsulare"
+  console.log("BAU")
+}
